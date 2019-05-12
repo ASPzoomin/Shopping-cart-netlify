9cd764fa710d4e97d0650d500eb61659bc856efb
pavan <pavanammula@gmail.com>
2019-05-12 15:36:36 +0530

    added clear-cart logic in cart

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ class UI {
                 class="product-img">
                 <button class="bag-btn" data-id=${product.id}>
                     <i class="fas fa-shopping-cart"></i>
-                    add to bag
+                    add to cart
                 </button>
             </div>
             <h3>${product.title}</h3>
@@ -126,6 +126,31 @@ class UI {
         cartOverlay.classList.remove('transparentBcg');
         cartDOM.classList.remove('showCart');
     }
+    cartLogic(){
+        clearCartBtn.addEventListener('click',()=>{
+            this.clearCart();
+        })
+    }
+    clearCart(){
+        let cartItems = cart.map(item => item.id);
+        cartItems.forEach(id =>this.removeItem(id));
+        while(cartContent.children.length>0){
+            cartContent.removeChild(cartContent.children[0])
+        }
+        this.hideCart();
+    }
+    removeItem(id){
+        cart = cart.filter(item => item.id !==id)
+        this.setCartValues(cart);
+        Storage.saveCart(cart);
+        let button = this.getSingleButton(id);
+        button.disabled = false;
+        button.innerHTML =`<i class="fas fa-shopping-cart">
+        </i>add to cart`;
+    }
+    getSingleButton(id){
+        return buttonsDOM.find(button =>button.dataset.id === id);
+    }
     
 }
 
@@ -155,5 +180,6 @@ document.addEventListener("DOMContentLoaded",()=>{
         Storage.saveProducts(products);
 }).then(() => {
     ui.getBagButton();
+    ui.cartLogic();
 })
 });
